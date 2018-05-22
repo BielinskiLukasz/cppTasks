19909b037df8f83b8d84dcd455bd8bc08551f374
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-05-22 20:06:26 +0200

    Practical tasl 03 added

---
+++ Allain Alex Przewodnik dla początkujących/practical tasks/zp03b - suma 2 liczb/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    double a=0;
+    double b=0;
+    cout << "Podaj dwie liczby do zdumowania (rozdzielone spacja):\n";
+    cin >> a >> b;
+    cout << "Suma to: " << a+b << endl;
+    return 0;
+}
---
+++ Allain Alex Przewodnik dla początkujących/practical tasks/zp03c - dzielenie 2 liczb/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    double a = 0, b = 0;
+    cout << "Podaj dzielna: ";
+    cin >> a;
+    cout << "\nPodaj dzielnik: ";
+    cin >> b;
+    cout << "\nWynik dzielenia to " << a/b << endl;
+    return 0;
+}
