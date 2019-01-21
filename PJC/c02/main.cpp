0c5b6981fdb99ca9c7bdc52c2bdedcb02f87111f
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2019-01-21 16:46:43 +0100

    Create PJC structure

---
+++ PJC/c02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout, std::endl;
+
+int main() {
+    // task01
+    cout << "Hello" << endl;
+
+    //task02
+    //task03
+    //task04
+    //task05
+    //task06
+    //task07
+}
