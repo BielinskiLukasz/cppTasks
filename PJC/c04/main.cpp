c2aba749d9650d66bdcc2261406988e8f7edc1dd
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2019-01-26 19:53:00 +0100

    Finish c04

--- PJC/c04/main.cpp
+++ PJC/c04/main.cpp
@@ -12,9 +12,23 @@ void dynamicArray();
 
 long mystrlen(char *tab);
 
-void copyArray(char *src, char *des, int size);
+void copyArray(const char *src, char *des, int size);
 
 void multipleWords();
+
+void printWords(char **, int);
+
+void intFun();
+
+void fun1(int wtr);
+
+void fun2(int *wtr);
+
+void fun3(int &wtr);
+
+void charFun();
+
+void fun(char &arg1, char *arg2);
 
 /**
  * Dany utwór powstał w wyniku realizowania procesu edukacyjnego w PJATK.
@@ -49,13 +63,13 @@ int main() {
     // zadeklaruj i zainicjuj zmienna wrt typu int,
     // wyswietl jej wartosc na ekran,
     // wywołaj funkcje fun1, fun2 i fun3 dostarczajac jako parametr wrt,
-    // ponownie wyswietl wartosc zmiennej wrt.
-
+    // ponownie wyswietl wartosc zmiennej wrt.2
+    intFun();
 
     //VI. Funkcja fun otrzyma jako parametry char& i char* do dwóch ciagów znaków. Zadaniem tej funkcji bedzie
     // znalezienie najdłuzszego podciagu. Jako wynik wyswietlone zostana wskazniki na poczatek i koniec najdłuzszego
     // podciagu.
-
+    charFun();
 
 }
 
@@ -96,7 +110,8 @@ void dynamicArray() {
 
     char *dynamicTab = new char[arraySize + 1];
     copyArray(inputData, dynamicTab, arraySize);
-    cout << dynamicTab;
+    cout << dynamicTab << endl;
+    delete[] dynamicTab;
 }
 
 long mystrlen(char *tab) {
@@ -105,7 +120,7 @@ long mystrlen(char *tab) {
     return --temp - tab;
 }
 
-void copyArray(char *src, char *des, int size) {
+void copyArray(const char *src, char *des, int size) {
     for (int i = 0; i < size; i++) {
         *(des + i) = *(src + i);
     }
@@ -113,9 +128,101 @@ void copyArray(char *src, char *des, int size) {
 }
 
 void multipleWords() {
-    //IV. Program z poprzedniego punktu nalezy wzbogacic o dynamicznie tworzona tablice przechowujaca wskazniki
-    // do wszystkich wprowadzonych słów. Wprowadzenie kolejnego słowa bedzie skutkowac wyswietleniem wszystkich
-    // wprowadzonych do tej pory słów. Jezeli w tablicy brakuje miejsca na kolejne słowa nalezy zwiekszyc rozmiar
-    // dotychczasowej tablicy dwukrotnie.
+    int dynamicTabOfTabsSize = 1;
+    char **dynamicTabOfTabs = new char *[dynamicTabOfTabsSize];
 
+    int wordsCounter = 0;
+    char inputData[100];
+    cout << "Input data: " << endl;
+    cin >> inputData;
+
+    while (*inputData != '0') {
+        long arraySize = mystrlen(inputData);
+
+        char *dynamicTab = new char[arraySize + 1];
+        copyArray(inputData, dynamicTab, arraySize);
+
+        if (wordsCounter >= dynamicTabOfTabsSize) {
+            dynamicTabOfTabsSize *= 2;
+            char **newTab = new char *[dynamicTabOfTabsSize];
+            for (int i = 0; i < dynamicTabOfTabsSize / 2; i++)
+                newTab[i] = dynamicTabOfTabs[i];
+            delete[] dynamicTabOfTabs;
+            dynamicTabOfTabs = newTab;
+        }
+        dynamicTabOfTabs[wordsCounter++] = dynamicTab;
+        printWords(dynamicTabOfTabs, wordsCounter);
+
+        cout << "Input data: " << endl;
+        cin >> inputData;
+    }
+
+    cout << "Array size " << dynamicTabOfTabsSize << endl
+         << "words in " << wordsCounter;
+    for (int i = 0; i < wordsCounter; i++) {
+        delete dynamicTabOfTabs[i];
+    }
+    delete[] dynamicTabOfTabs;
+}
+
+void printWords(char **words, int counter) {
+    for (int i = 0; i < counter; i++) {
+        cout << words[i] << endl;
+    }
+}
+
+void intFun() {
+    int wtr = 0;
+    cout << "Before " << wtr << endl;
+    fun1(wtr);
+    cout << "After fun1 " << wtr << endl;
+    fun2(&wtr);
+    cout << "After fun2 " << wtr << endl;
+    fun3(wtr);
+    cout << "After fun3 " << wtr << endl;
+}
+
+void fun1(int wtr) {
+    cout << "fun 1 before " << wtr << endl;
+    wtr++;
+    cout << "fun1 after " << wtr << endl;
+}
+
+void fun2(int *wtr) {
+    cout << "fun 2 before " << *wtr << endl;
+    (*wtr)++;
+    cout << "fun2 after " << *wtr << endl;
+}
+
+void fun3(int &wtr) {
+    cout << "fun 3 before " << wtr << endl;
+    wtr++;
+    cout << "fun3 after " << wtr << endl;
+}
+
+void charFun() {
+    //VI. Funkcja fun otrzyma jako parametry char& i char* do dwóch ciagów znaków. Zadaniem tej funkcji bedzie
+    // znalezienie najdłuzszego podciagu. Jako wynik wyswietlone zostana wskazniki na poczatek i koniec najdłuzszego
+    // podciagu.
+    char *shortSentence = "It's short, isn't it?";
+    char *longSentence = "It's long sentence, isn't it?";
+
+    fun(*shortSentence, longSentence);
+}
+
+void fun(char &arg1, char *arg2) {
+    long arg1Length = mystrlen(&arg1);
+    cout << "Size of first arg is " << arg1Length << endl;
+    long arg2Length = mystrlen(arg2);
+    cout << "Size of second arg is " << arg2Length << endl;
+
+    char *longestSentence = arg1Length > arg2Length ? &arg1 : arg2;
+    long longestSize = arg1Length > arg2Length ? arg1Length : arg2Length;
+
+    char *startOfLongestSentence = longestSentence;
+    char *endOfLongestSentence = (longestSentence + longestSize - 1);
+
+    cout << "Longest sentence is " << longestSentence << " with size " << longestSize << endl
+         << "Starts: " << startOfLongestSentence << endl
+         << "Ends: " << endOfLongestSentence << endl;
 }
